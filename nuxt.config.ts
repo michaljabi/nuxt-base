8df1e4ea23cf2b1acfc33ff64e75548df20ad96b
Jabi <michal.jabi@gmail.com>
2023-10-04 01:09:37 +0200

    fix: /nuxt-base/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+console.log('I GOT', process.env.NODE_ENV);
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: '/nuxt-base/',
+    baseURL: process.env.NODE_ENV === 'development' ? '/' : '/nuxt-base/',
   }
 })
